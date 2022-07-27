2e15c4e529cfdb53b565f0eaacaf162335ced2e9
jkkjkhh <jkkjkhh@gmail.com>
2022-07-27 10:35:09 +0900

    프로젝트 기본 셋팅

---
+++ src/test/java/com/team/chatproject/ChatprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.chatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
