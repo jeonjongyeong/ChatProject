ec470a82fabeb4c598bcceb22cafe4e44398b986
2timitu <hmhnes@gmail.com>
2022-08-03 10:27:27 +0900

    s

--- src/main/java/com/team/chatproject/controller/MainControllor.java
+++ src/main/java/com/team/chatproject/controller/MainControllor.java
@@ -1,13 +1,21 @@
 package com.team.chatproject.controller;
 
+import com.team.chatproject.domain.Rq;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainControllor {
+
+    @Autowired
+    private Rq rq;
     // 메인 페이지
     @RequestMapping("/")
     public String main() {
+        if(!rq.isLogin()){
+            return "/member/login";
+        }
         return "/article/list";
     }
 
