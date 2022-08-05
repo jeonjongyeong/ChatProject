8bacaebf9cf5c241f6b89740872c30e931899994
jkkjkhh <jkkjkhh@gmail.com>
2022-08-05 09:29:56 +0900

    댓글 역순 출력

--- src/main/java/com/team/chatproject/service/ArticleService.java
+++ src/main/java/com/team/chatproject/service/ArticleService.java
@@ -1,13 +1,16 @@
 package com.team.chatproject.service;
 
 import com.team.chatproject.domain.Article;
+import com.team.chatproject.domain.Comment;
 import com.team.chatproject.domain.ResultData;
 import com.team.chatproject.repository.ArticleRepository;
 import com.team.chatproject.util.DataNotFoundException;
+import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +29,9 @@ public class ArticleService {
         Optional<Article> opArticle = this.articleRepository.findById(id);
         if (opArticle.isPresent()) {
             Article article = opArticle.get();
+            List<Comment> commentList = article.getCommentList();
+            Collections.reverse(commentList);
+            article.setCommentList(commentList);
             article.setViewCount(article.getViewCount() + 1);
             this.articleRepository.save(article);
             return article;
